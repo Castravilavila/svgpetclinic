f8135acb39431099fdd7082ac84bf28ac969e966
castravila <sandualist@gmail.com>
2019-12-23 16:46:25 +0200

    adding release plugin

--- pet-clinic-web/src/test/java/svgpetclinic/SvgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/svgpetclinic/SvgPetClinicApplicationTests.java
@@ -1,11 +1,8 @@
 package svgpetclinic;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SvgPetClinicApplicationTests {
-
-
 
 }
