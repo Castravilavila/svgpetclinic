04bffa33c2c81e24ccdf748c94f9009178db47ae
castravila <sandualist@gmail.com>
2020-01-31 14:42:40 +0200

    corrected changes. Closes #4

--- pet-clinic-web/src/test/java/com/castravet/svgpetclinic/SvgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/com/castravet/svgpetclinic/SvgPetClinicApplicationTests.java
@@ -2,13 +2,14 @@ package com.castravet.svgpetclinic;
 
 
 
+import com.castravet.sfgpetclinic.SvgPetClinicApplication;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest
+@SpringBootTest(classes = SvgPetClinicApplication.class)
 class SvgPetClinicApplicationTests {
 
     @Test
